a618039ede927867004fecc0349e77273711de73
Arthur Conti <arthurgallotticonti@gmail.com>
2022-06-02 00:58:30 -0300

    finishing creating user and delete from the frontend

--- codes/dockerCompose-example/backend/server.js
+++ codes/dockerCompose-example/backend/server.js
@@ -9,6 +9,7 @@ const port = 3333;
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers","Content-Type");
   app.use(cors());
   next();
 });
--- codes/dockerCompose-example/frontend-vue/src/components/TableUser.vue
+++ codes/dockerCompose-example/frontend-vue/src/components/TableUser.vue
@@ -1,20 +1,33 @@
 <template>
-  <n-data-table
-    remote
-    :columns="columns"
-    :data="data"
-    :pagination="pagination"
-    :loading="loading"
-    :bordered="true"
-  ></n-data-table>
+  <n-space id="container">
+    <n-data-table
+      remote
+      :columns="columns"
+      :data="data"
+      :pagination="pagination"
+      :loading="loading"
+      :bordered="false"
+      class="dataTable"
+    ></n-data-table>
+    <n-button circle @click="addPerson">
+      <template #icon>
+        <n-icon><plus-icon /></n-icon>
+      </template>
+    </n-button>
+  </n-space>
 </template>
 
 <script>
-import { h, defineComponent } from "vue";
-import { NButton, NDataTable } from "naive-ui";
+import { h, defineComponent, ref, onMounted } from "vue";
+import { NButton, NDataTable, NIcon } from "naive-ui";
+import { Add12Filled as PlusIcon } from "@vicons/fluent";
 import Api from "../provider/api.provider";
+import Swal from "sweetalert2";
 
-const createColumns = ({ play }) => {
+const dataRef = ref([]);
+const loadingRef = ref(true);
+
+const createColumns = () => {
   return [
     {
       title: "Id",
@@ -29,48 +42,122 @@ const createColumns = ({ play }) => {
       key: "idade",
     },
     {
-      title: "Action",
-      key: "actions",
+      title: "",
+      key: "delete",
       render(row) {
         return h(
           NButton,
           {
             strong: true,
-            tertiary: true,
+            primary: true,
+            color: "red",
             size: "small",
-            onClick: () => play(row),
+            onClick: () => {
+              Api.delete(`/pessoa/${parseInt(row.id)}`)
+                .then(() => {
+                  Api.get("/pessoas")
+                    .then((response) => {
+                      dataRef.value = response.data;
+                    })
+                    .catch((e) => console.error(e.message));
+                  Swal.fire(
+                    "Sucesso",
+                    `Usuario ${row.nome} foi deletado com sucesso`,
+                    "success"
+                  );
+                })
+                .catch((e) => console.error(e.message));
+            },
           },
-          { default: () => "Play" }
+          { default: () => "Deletar" }
         );
       },
     },
   ];
 };
 
-const data = [];
-Api.get("/pessoas")
-  .then((response) => {
-    response.data.forEach((dataResponse) => {
-      data.push(dataResponse);
-    });
-  })
-  .catch((e) => console.error(e.message));
-
-console.log(data);
 export default defineComponent({
   components: {
     NDataTable,
+    PlusIcon,
+    NButton,
+    NIcon,
+  },
+  methods: {
+    async addPerson() {
+      const { value: formValues } = await Swal.fire({
+        title: "Cadastrar usuário",
+        html:
+          '<input id="nome" placeholder="Nome" class="swal2-input">' +
+          '<input id="idade" placeholder="Idade" class="swal2-input">',
+        focusConfirm: false,
+        preConfirm: () => {
+          return {
+            nome: document.getElementById("nome").value,
+            idade: parseInt(document.getElementById("idade").value),
+          };
+        },
+      });
+
+      if (formValues) {
+        loadingRef.value = true;
+        Api.post("/pessoa", formValues)
+          .then((response) => {
+            Api.get("/pessoas")
+              .then((response) => {
+                dataRef.value = response.data;
+              })
+              .catch((e) => console.error(e.message))
+              .finally(() => {
+                loadingRef.value = false;
+                Swal.fire(
+                  `${response.data.message}`,
+                  "Usuário cadastrado com sucesso",
+                  "success"
+                );
+              });
+          })
+          .catch((e) => {
+            console.error(e.message);
+          });
+      }
+    },
   },
   setup() {
+    onMounted(() => {
+      loadingRef.value = true;
+      Api.get("/pessoas")
+        .then((response) => {
+          dataRef.value = response.data;
+        })
+        .catch((e) => console.error(e.message))
+        .finally(() => {
+          loadingRef.value = false;
+        });
+    });
+
     return {
-      data,
-      columns: createColumns({
-        play(row) {
-          console.log(`Play ${row.nome}`);
-        },
-      }),
+      data: dataRef,
+      loading: loadingRef,
+      columns: createColumns({}),
       pagination: false,
     };
   },
 });
 </script>
+
+<style scoped>
+#container {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.dataTable {
+  width: 70%;
+  height: 70%;
+}
+</style>
