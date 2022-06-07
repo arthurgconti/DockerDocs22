7fd59c60a49eee9dcf67538488eb2825db1842e2
Arthur Conti <arthurgallotticonti@gmail.com>
2022-06-07 00:50:01 -0300

    configurating options to vue frontend app for production and creating his image to container

---
+++ codes/dockerCompose-example/frontend-vue/vue.config.js
@@ -0,0 +1,13 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+
+const defineConfig = {
+  // options...
+  publicPath: "./",
+  crossorigin: "anonymous",
+};
+
+export default defineConfig;
